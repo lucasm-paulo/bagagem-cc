d00106d0665f46643ea08454cb11fcf0ca263819
Lucas Mendes Paulo <103695051+lucasm-paulo@users.noreply.github.com>
2023-06-12 17:12:46 -0300

    Update renameProject.sh

--- renameProject.sh
+++ renameProject.sh
@@ -12,5 +12,5 @@ old_project_name="cc-tools-demo"
 new_project_name="$2"
 new_profile_name="$1"
 
-find . -type f -not -path '*/\.git*' -not -path '*/node_modules*' -exec sed -i "s/$old_project_name/$new_project_name/g" {} +
 find . -type f -not -path '*/\.git*' -not -path '*/node_modules*' -exec sed -i "s/goledgerdev\/$old_project_name/$new_profile_name\/$new_project_name/g" {} +
+find . -type f -not -path '*/\.git*' -not -path '*/node_modules*' -exec sed -i "s/$old_project_name/$new_project_name/g" {} +
