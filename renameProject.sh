e14558cbf63eb6d7f4e75c511e64cf33129c4d96
Lucas Mendes Paulo <103695051+lucasm-paulo@users.noreply.github.com>
2023-06-12 16:55:47 -0300

    Update renameProject.sh

--- renameProject.sh
+++ renameProject.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # CAUTION: this script will replace every occurrence of the word
-# `GoLedgerDev/cc-tools-demo` in the project folder with whatever argument
+# `goledgerdev/cc-tools-demo` in the project folder with whatever argument
 # you pass. Be very careful.
 
 if [ $# -lt 1 ] ; then
-  printf "Usage:\n$ ./renameProject.sh <repo-name/my-project-name>\n"
+  printf "Usage:\n$ ./renameProject.sh <profile-name/my-project-name>\n"
   exit
 fi
 
-grep -rl GoLedgerDev/cc-tools-demo . --exclude-dir={.git,node_modules} | xargs sed -i "s|GoLedgerDev/cc-tools-demo|$1|g"
+grep -rl goledgerdev/cc-tools-demo . --exclude-dir={.git,node_modules} | xargs sed -i "s/goledgerdev\/cc-tools-demo/$1/g"
